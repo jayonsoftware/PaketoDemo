6d9ca3ac4b6e2e46606279738bc0cfe0943a054a
Jay <jay@jayonsoftware.com>
2023-04-27 17:42:02 -0400

    Added SriLog

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.PortableExecutable;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace aspnet
 {
@@ -14,14 +16,40 @@ namespace aspnet
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+  // Add console (Sink) as logging target
+  .WriteTo.Console()
+
+    // Set default minimum log level
+    .MinimumLevel.Debug()
+
+    // Create the actual logger
+    .CreateLogger();
+
             var builder = CreateHostBuilder(args);
+
+          
+
+
+
+            builder.UseSerilog(Log.Logger);
+
+
+
             var host = builder.Build();
-            host.Run();    
-           
+
+
+            host.Run();
+
+ 
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
+
+
+            Log.Information(Environment.MachineName);
             var builder = new WebHostBuilder();
 
             var portVar = Environment.GetEnvironmentVariable("PORT");
@@ -29,16 +57,14 @@ namespace aspnet
             {
                 return Host.CreateDefaultBuilder(args)
                     .ConfigureWebHostDefaults(webBuilder =>
-                    {
-              
+                    {          
                         webBuilder.UseStartup<Startup>();
                     });
 
             } else
-            {
+            { 
 
-
-                return Host.CreateDefaultBuilder(args)
+            return Host.CreateDefaultBuilder(args)
                     .ConfigureWebHostDefaults(webBuilder =>
                     {
                         webBuilder.UseStartup<Startup>();
